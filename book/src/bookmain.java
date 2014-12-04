738eb2853094c63a6b98208760d0ab3640b5eae7
Bong <bongmin92@gmail.com>
2014-12-04 21:07:27 +0900

    +mainmenu

--- book/src/bookmain.java
+++ book/src/bookmain.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
-public class bookmain {
+public class bookmain{
+	public static void main(String[] args){
+		int menu;
+		Scanner scan = new Scanner(System.in);
 
+		System.out.println("------------------------------");
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		System.out.println("3. 종료");
+		System.out.println("------------------------------");
+		System.out.print("메뉴 입력:");
+		menu = scan.nextInt();
+
+	}
 }
