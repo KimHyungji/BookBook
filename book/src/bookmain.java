54db4a9ac0bc9b3b66128aec488cb4459fe0b6be
KimHyungji <hyungjee91@gmail.com>
2014-12-19 02:37:51 +0900

    그냥

--- book/src/bookmain.java
+++ book/src/bookmain.java
@@ -163,84 +163,83 @@ public class bookmain{
 			start.show_menu();
 	}
 		//초기화면 
-		public void show_menu() throws Exception{
-			int menu;
-			System.out.println("------------------------------");
-			System.out.println("1. 로그인");
-			System.out.println("2. 회원가입");
-			System.out.println("3. 종료");
-			System.out.println("------------------------------");
-			System.out.print("메뉴 입력:");
-			menu = scan.nextInt();
+	public void show_menu() throws Exception{
+		int menu;
+		System.out.println("------------------------------");
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		System.out.println("3. 종료");
+		System.out.println("------------------------------");
+		System.out.print("메뉴 입력:");
+		menu = scan.nextInt();
 
-			switch(menu){
-			case 1:
-				login();
-				break;
-			case 2:
-				join();
-				break;
-			case 3:
-				the_end();
-				break;
-			}
+		switch(menu){
+		case 1:
+			login();
+			break;
+		case 2:
+			join();
+			break;
+		case 3:
+			the_end();
+			break;
 		}
-	
-		//로그인
-		public void login() throws Exception{
-			Member loginmem = new Member();
-			int id;
-			int loginputcomplete =0;
-			do{
-				System.out.println("[아이디 입력]");
-				id = scan.nextInt();
-				
-				//사서의 경우 사서의 아이디는 123으로 지정
-				if(id != 123){
-				if(loginmem.getequal(id)==-1){
-					System.out.println("존재하지 않는 아이디입니다.");
-					loginputcomplete = 1;
-				}
-				else
-					loginputcomplete = 0;
-				}
-				loginputcomplete = 0;
-			}while(loginputcomplete ==1);
+	}
+
+	//로그인
+	public void login() throws Exception{
+		Member loginmem = new Member();
+		int id;
+		int loginputcomplete =0;
+		do{
+			System.out.println("[아이디 입력]");
+			id = scan.nextInt();
 			
-			System.out.println("[비밀번호 입력]");
-			scan.nextLine(); 
-			String password = scan.nextLine();
-			
-			//사서의 경우 비밀번호 rootpass로 지정
-			if(id == 123 && password.equals("rootpass"))
-			{
-				//사서메뉴 출력
-				librarian_menu();
+			//사서의 경우 사서의 아이디는 123으로 지정
+			if(id != 123){
+			if(loginmem.getequal(id)==-1){
+				System.out.println("존재하지 않는 아이디입니다.");
+				loginputcomplete = 1;
 			}
 			else
-			{
-				//‘로그인에 실패하였습니다. 아이디와 패스워드를 다시 입력해주세요.’메시지출력
-				System.out.println("로그인에 실패하였습니다. 아이디와 패스워드를 다시 입력해주세요.");
-				//다시 로그인 화면
-				show_menu();
+				loginputcomplete = 0;
 			}
-			
-			//학생의 경우 비밀번호가 파일에 있으면 ‘로그인 되었습니다.’라는 메시지를 출력
-			//학생 메뉴 출력 
-
+			loginputcomplete = 0;
+		}while(loginputcomplete ==1);
+		
+		System.out.println("[비밀번호 입력]");
+		scan.nextLine(); 
+		String password = scan.nextLine();
+		
+		//사서의 경우 비밀번호 rootpass로 지정
+		if(id == 123 && password.equals("rootpass"))
+		{
+			//사서메뉴 출력
+			librarian_menu();
 		}
+		else
+		{
+			//‘로그인에 실패하였습니다. 아이디와 패스워드를 다시 입력해주세요.’메시지출력
+			System.out.println("로그인에 실패하였습니다. 아이디와 패스워드를 다시 입력해주세요.");
+			//다시 로그인 화면
+			show_menu();
+		}
+		
+		//학생의 경우 비밀번호가 파일에 있으면 ‘로그인 되었습니다.’라는 메시지를 출력
+		//학생 메뉴 출력 
+
+	}
 	
 		//회원가입
 	public void join() throws Exception{
-
 		System.out.println("[회원가입 정보 입력]");
 
 		Member newmem = new Member();
 		scan.nextLine();  // 자바 Scanner의 문제로 불필요한 개행문자를 제거하기 위해 임시로 넣은 코드임
 		int newID,inputcomplete=0;
 		do{
-		System.out.print("아이디(학번):");
-		newID = scan.nextInt();
+			System.out.print("아이디(학번):");
+			newID = scan.nextInt();
 		if(newmem.getequal(newID)==1){
 			System.out.println("이미 사용중인 아이디입니다.");
 		}
@@ -256,74 +255,76 @@ public class bookmain{
 		inputcomplete = 0;
 		String newpassword;
 		do{
-		System.out.print("비밀번호:");
-		scan.nextLine();
-		newpassword = scan.nextLine();
+			System.out.print("비밀번호:");
+			scan.nextLine();
+			newpassword = scan.nextLine();
 		//비밀먼호는 문자,숫자,기호 제한X
-		//8자리 이상 입력해야한다.
-		if(newpassword.length() < 8)
-			System.out.println("8자 이상의 패스워드를 입력해주세요.");
-		else
-			inputcomplete = 1;
-		//사용자가 패스워드에 8자 미만을 입력하였다면 ‘8자 이상의 패스워드를 입력해주세요.’ 메시지 출력
-		}while(inputcomplete == 0);
+			//사용자가 패스워드에 8자 미만을 입력하였다면 
+			if(newpassword.length() < 8)
+			//‘8자 이상의 패스워드를 입력해주세요.’ 메시지 출력
+				System.out.println("8자 이상의 패스워드를 입력해주세요.");
+			else
+				inputcomplete = 1;
+			}while(inputcomplete == 0);
 		
 		do{
-		System.out.print("비밀번호 확인:");
-		String againpassword = scan.nextLine();
-		//패스워드를 2번 입력받아 두 내용이 일치하는지 확인한다.
-		if(againpassword.equals(newpassword)){
-			newmem.setpassword(newpassword);
-			inputcomplete = 1;
-		}
-		else
-			System.out.println("패스워드가 일치하지 않습니다. 다시 확인하고 입력해주세요.");
-		}while(inputcomplete ==0);
-		//두 내용이 일치하지 않을 경우 않는다면 
-		//‘패스워드가 일치하지 않습니다. 다시 확인하고 입력해주세요.’ 메시지 출력
+			//패스워드를 2번 입력받아 두 내용이 일치하는지 확인한다.
+			System.out.print("비밀번호 확인:");
+			String againpassword = scan.nextLine();
+			
+			if(againpassword.equals(newpassword)){
+				newmem.setpassword(newpassword);
+				inputcomplete = 1;
+			}
+			else
+				//두 내용이 일치하지 않을 경우 않는다면 
+				
+				//‘패스워드가 일치하지 않습니다. 다시 확인하고 입력해주세요.’ 메시지 출력
+				System.out.println("패스워드가 일치하지 않습니다. 다시 확인하고 입력해주세요.");
+			}while(inputcomplete ==0);
+	
 		inputcomplete = 0;
 		do{
-		System.out.print("이름:");
-		String newname = scan.nextLine();
-		//이름 입력은 두 자 이상이어야 한다.
-		if(newname.length() < 2)
-			System.out.println("두 글자 이상 입력해주세요.");
-		else{
-			newmem.setname(newname);
-			inputcomplete = 1;
-		}
-		}while(inputcomplete == 0);
-		//두 글자 미만으로 입력되었을 경우, ‘두 글자 이상 입력해주세요.’
+			System.out.print("이름:");
+			String newname = scan.nextLine();
+			//두 글자 미만으로 입력되었을 경우
+			if(newname.length() < 2)
+				System.out.println("두 글자 이상 입력해주세요.");
+			else{
+				newmem.setname(newname);
+				inputcomplete = 1;
+			}
+			}while(inputcomplete == 0);
+			
 		inputcomplete = 0;
 		do{
-		System.out.print("학과:");
-		String newmajor = scan.nextLine();
-		//학과 입력은 두 자 이상이어야 한다.
-		//두 글자 미만으로 입력되었을 경우, ‘두 글자 이상 입력해주세요.’
-		if(newmajor.length() < 2)
-			System.out.println("두 글자 이상 입력해주세요.");
-		else{
-			newmem.setmajor(newmajor);
-			inputcomplete =1;
-		}
-		}while(inputcomplete == 0);
+			System.out.print("학과:");
+			String newmajor = scan.nextLine();
+			//학과 입력은 두 자 이상이어야 한다.
+			//두 글자 미만으로 입력되었을 경우, ‘두 글자 이상 입력해주세요.’
+			if(newmajor.length() < 2)
+				System.out.println("두 글자 이상 입력해주세요.");
+			else{
+				newmem.setmajor(newmajor);
+				inputcomplete =1;
+			}
+			}while(inputcomplete == 0);
 		//회원가입에 성공할 시 '회원 가입 완료'메시지 출력
 		System.out.println("회원가입 완료!");
+		
 		//collection.addmem(newmem);
 		//collection.add(mem);
 		//memberCount++;
 		//메시지 출력과 동시에 로그인 화면으로 간다.
 		login();
 		
-	
-	}
-	
-	//종료
+		}
+		//종료
 	public void the_end(){
 		System.exit(-1);
 		//더 이상의 메뉴를 출력시키지 않는다.
 		
-	}
+		}
 	
 	//학생의 메뉴
 	public void student_menu() throws Exception{
@@ -416,62 +417,62 @@ public class bookmain{
 		String newtitle;
 		int inputcomplete2=0;
 		do{
-		System.out.print("제목:");
-		newtitle = scan.nextLine();
-			newbook.settitle(newtitle);
-			inputcomplete2 =1;
-		}while(inputcomplete2 ==0);
+			System.out.print("제목:");
+			newtitle = scan.nextLine();
+				newbook.settitle(newtitle);
+				inputcomplete2 =1;
+			}while(inputcomplete2 ==0);
 		
 		inputcomplete2 = 0;
 		String newauthor;
 		do{
-		System.out.print("저자:");
-		newauthor = scan.nextLine();
-		newbook.setauthor(newauthor);
-			inputcomplete2 = 1;
-		}while(inputcomplete2 == 0);
+			System.out.print("저자:");
+			newauthor = scan.nextLine();
+			newbook.setauthor(newauthor);
+				inputcomplete2 = 1;
+			}while(inputcomplete2 == 0);
 		
 		inputcomplete2 = 0;
 		String newpublish;
 		do{
-		System.out.print("출판사:");
-		newpublish = scan.nextLine();
-		newbook.setpublish(newpublish);
-			inputcomplete2 = 1;
-		}while(inputcomplete2 == 0);
+			System.out.print("출판사:");
+			newpublish = scan.nextLine();
+			newbook.setpublish(newpublish);
+				inputcomplete2 = 1;
+			}while(inputcomplete2 == 0);
 		
 		inputcomplete2 = 0;
 		int newISBN;
 		do{
-		System.out.print("ISBN:");
-		newISBN = scan.nextInt();
-		if(newbook.getequal(newISBN)==1){
-			System.out.println("이미 등록된 ISBN입니다.");
-		}
-		else{
-			newbook.setISBN(newISBN);
-			inputcomplete2 = 1;
-		}
-		}while(inputcomplete2 == 0);
+			System.out.print("ISBN:");
+			newISBN = scan.nextInt();
+			if(newbook.getequal(newISBN)==1){
+				System.out.println("이미 등록된 ISBN입니다.");
+			}
+			else{
+				newbook.setISBN(newISBN);
+				inputcomplete2 = 1;
+			}
+			}while(inputcomplete2 == 0);
 		
 		inputcomplete2 = 0;
 		String newavail;
 		do{
-		System.out.print("대출 여부:");
-		newavail = scan.nextLine();
-		scan.nextLine();
-		newbook.setavail(newavail);
-			inputcomplete2 = 1;
-		}while(inputcomplete2 == 0);
-		
+			System.out.print("대출 여부:");
+			newavail = scan.nextLine();
+			scan.nextLine();
+			newbook.setavail(newavail);
+				inputcomplete2 = 1;
+			}while(inputcomplete2 == 0);
+			
 		inputcomplete2 = 0;
 		int newborrower;
 		do{
-		System.out.print("대여자:");
-		newborrower = scan.nextInt();
-		newbook.setborrower(newborrower);
-			inputcomplete2 = 1;
-		}while(inputcomplete2 == 0);
+			System.out.print("대여자:");
+			newborrower = scan.nextInt();
+			newbook.setborrower(newborrower);
+				inputcomplete2 = 1;
+			}while(inputcomplete2 == 0);
 		
 		System.out.println("도서 등록 완료!");
 		librarian_menu();
